fba0509f3a5f94c95cf3c5725c526ffe80b9eb8a
Gino Mazza <mazza.gino32@gmail.com>
2025-09-15 09:58:36 -0300

    corrected method names

--- calculator_functions.py
+++ calculator_functions.py
@@ -1,18 +1,25 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+
+
+def sumar(a, b):
+    return a + b
+
+
+def restar(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    return a / b
+
+
 print("----- PYTHON CALCULATOR -----")
-
-def Sumar(a, b):
-     return a + b
-
-def Restar(a, b):
-     return a - b
-
-def Multiplicar(a, b):
-     return a * b
-
-def Dividir(a, b):
-     return a / b
-          
-print("El resultado de la suma de 10 y 5 es:", Sumar(10, 5))
-print("El resultado de la resta de 10 y 5 es:", Restar(10, 5))
-print("El resultado de la multiplicacion de 10 y 5 es:", Multiplicar(10, 5))
-print("El resultado de la division de 10 y 5 es:", Dividir(10, 5))
+print("El resultado de la suma de 10 y 5 es:", sumar(10, 5))
+print("El resultado de la resta de 10 y 5 es:", restar(10, 5))
+print("El resultado de la multiplicacion de 10 y 5 es:", multiplicar(10, 5))
+print("El resultado de la division de 10 y 5 es:", dividir(10, 5))
