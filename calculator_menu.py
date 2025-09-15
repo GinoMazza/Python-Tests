97d7499c6c3f97838cfb80b3f4142542810884eb
Gino Mazza <mazza.gino32@gmail.com>
2025-09-15 08:42:57 -0300

    created calculator class with functional menu

--- calculator_menu.py
+++ calculator_menu.py
@@ -1,41 +1,73 @@
-def Sumar(a, b):
-     return a + b
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+import os
+import time
 
-def Restar(a, b):
-     return a - b
 
-def Multiplicar(a, b):
-     return a * b
+class Calculadora:
+    def sumar(self, a, b):
+        return a + b
 
-def Dividir(a, b):
-     return a / b
+    def restar(self, a, b):
+        return a - b
 
-print("----- PYTHON CALCULATOR -----")
-print("1- Sumar")
-print("2- Restar")
-print("3- Multiplicar")
-print("4- Dividir")
-print("0- Salir\n")
+    def multiplicar(self, a, b):
+        return a * b
 
-opcion = input("Seleccione una opcion: ")
-match opcion:
-     case "1":
-          primerNumero = float(input("Primer numero: "))
-          segundoNumero = float(input("Primer numero: "))
-          print("Resultado:", Sumar(primerNumero, segundoNumero))
-     case "2":
-          primerNumero = float(input("Primer numero: "))
-          segundoNumero = float(input("Primer numero: "))
-          print("Resultado:", Restar(primerNumero, segundoNumero))
-     case "3":
-          primerNumero = float(input("Primer numero: "))
-          segundoNumero = float(input("Primer numero: "))
-          print("Resultado:", Multiplicar(primerNumero, segundoNumero))
-     case "4":
-          primerNumero = float(input("Primer numero: "))
-          segundoNumero = float(input("Primer numero: "))
-          print("Resultado:", Dividir(primerNumero, segundoNumero))
-     case "0":
-          print("Saliendo...")
-     case _:
-          print("Opción no válida")
+    def dividir(self, a, b):
+        return a / b
+
+    def mostrar_menu(self):
+        os.system("cls")
+        print("----- PYTHON CALCULATOR -----")
+        print("1- Sumar")
+        print("2- Restar")
+        print("3- Multiplicar")
+        print("4- Dividir")
+        print("0- Salir\n")
+        opcion = input("Seleccione una opcion: ")
+        match opcion:
+            case "1":
+                primer_numero = float(input("Primer numero: "))
+                segundo_numero = float(input("Segundo numero: "))
+                print("Resultado:", self.sumar(primer_numero, segundo_numero))
+                input()
+                os.system("cls")
+                self.mostrar_menu()
+            case "2":
+                primer_numero = float(input("Primer numero: "))
+                segundo_numero = float(input("Segundo numero: "))
+                print("Resultado:", self.restar(primer_numero, segundo_numero))
+                input()
+                os.system("cls")
+                self.mostrar_menu()
+            case "3":
+                primer_numero = float(input("Primer numero: "))
+                segundo_numero = float(input("Segundo numero: "))
+                print("Resultado:", self.multiplicar(
+                    primer_numero, segundo_numero))
+                input()
+                os.system("cls")
+                self.mostrar_menu()
+            case "4":
+                primer_numero = float(input("Primer numero: "))
+                segundo_numero = float(input("Segundo numero: "))
+                print("Resultado:", self.dividir(
+                    primer_numero, segundo_numero))
+                input()
+                os.system("cls")
+                self.mostrar_menu()
+            case "0":
+                os.system("cls")
+                print("Saliendo...")
+                time.sleep(1)
+                return
+            case _:
+                print("Opción no válida")
+                input()
+                os.system("cls")
+                self.mostrar_menu()
+
+
+calc = Calculadora()
+calc.mostrar_menu()
